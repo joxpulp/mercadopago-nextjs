6075f8473a3d134a2384788eae876c5cf4e31ea9
JOSUE <joxpulp@gmail.com>
2023-08-01 13:12:41 -0300

    fix:

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['via.placeholder.com']
+  },
+  experimental: {
+    nextScriptWorkers: true
+  },
   transpilePackages: ['@mercadopago/sdk-react']
 }
 
